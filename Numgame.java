dc19c4205b24c84c2b89a902756449871b5a656e
Kamidi Vishnuvardhan Reddy <146474328+Vishnureddie07@users.noreply.github.com>
2023-10-02 12:31:52 +0530

    Updated Numgame.java

--- Numgame.java
+++ Numgame.java
@@ -1,14 +1,10 @@
-
-
 import java.util.Random;
 import java.util.Scanner;
-
 
 public class Numgame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        
         int minRange = 1;
         int maxRange = 100;
         int maxAttempts = 10;
@@ -19,20 +15,16 @@ public class Numgame {
         while (true) {
             int targetNumber = random.nextInt(maxRange - minRange + 1) + minRange;
             int attempts = 0;
-            
             System.out.println("\nI've selected a number between " + minRange + " and " + maxRange + ". Try to guess it!");
             
             while (attempts < maxAttempts) {
                 System.out.print("Enter your guess: ");
                 int Guess = scanner.nextInt();
-                
                 if (Guess < minRange || Guess > maxRange) {
                     System.out.println("Try again, You are out of range.");
                     continue;
                 }
-                
                 attempts++;
-                
                 if (Guess == targetNumber) {
                     System.out.println("Congratulations! You guessed the correct number (" + targetNumber + ") in " + attempts + " attempts.");
                     score += maxAttempts - attempts + 1;
@@ -43,7 +35,6 @@ public class Numgame {
                     System.out.println("Too high! Remaining Attempts: " + (maxAttempts - attempts));
                 }
             }
-            
             System.out.print("\nDo you want to play another round? (yes/no): ");
             String playAgain = scanner.next();
             
@@ -51,7 +42,6 @@ public class Numgame {
                 break;
             }
         }
-        
         System.out.println("Thanks for playing! Your total score is: " + score);
         scanner.close();
     }
